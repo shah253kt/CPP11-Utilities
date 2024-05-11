d3a66570410a4478684d194f43dd52289ed3c831
Ahmad Shahril <shah25392@gmail.com>
2024-05-11 14:23:56 +0800

    Minor update

--- examples/MathUtilities.ino
+++ examples/MathUtilities.ino
@@ -3,7 +3,7 @@
 void setup()
 {
     auto a = MathUtilities::equals(1.234, 1.2345);      // Returns true
-    auto b = MathUtilities::equals(1.234, 1.2345, 0.1); // Returns false
+    auto b = MathUtilities::equals(1.234, 1.3345, 0.1); // Returns false
     auto c = MathUtilities::equals(1.234, 1.24);        // Returns false
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #ifndef UNIT_TEST
 
 #include "StringUtilities.h"
+#include "MathUtilities.h"
 
 #include <Arduino.h>
 
@@ -13,10 +14,15 @@ void setup()
 
 void loop()
 {
-    Serial.println("\n----------------");
+    Serial.println("\n--------String Utilities--------");
     Serial.printf("ltrim result: \'%s\'\n", StringUtilities::ltrim("    \r  \n My Actual Data \r \n   ").c_str());
     Serial.printf("rtrim result: \'%s\'\n", StringUtilities::rtrim("    \r  \n My Actual Data \r \n   ").c_str());
     Serial.printf("trim result: \'%s\'\n", StringUtilities::trim("    \r  \n My Actual Data \r \n   ").c_str());
+
+    Serial.println("\n--------Math Utilities--------");
+    Serial.printf("1.234 == 1.2345 (with epsilon = 0.001) : %s\n", MathUtilities::equals(1.234, 1.2345) ? "true" : "false");
+    Serial.printf("1.234 == 1.245 (with epsilon = 0.01): %s\n", MathUtilities::equals(1.234, 1.245, 0.01) ? "true" : "false");
+    Serial.printf("1.234 == 1.23 (with epsilon = 0.001) : %s", MathUtilities::equals(1.234, 1.23) ? "true" : "false");
 
     delay(5000);
 }
